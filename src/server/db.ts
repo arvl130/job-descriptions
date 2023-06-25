2d80ec163efc427337a2ced4ab0d66bf0d4a7ef7
Angelo Geulin <angelogeulin123@gmail.com>
2023-06-25 11:02:35 +0800

    refactor: move postgresql db object initialization to its own file

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,45 +1,7 @@
-import { Kysely, PostgresDialect } from "kysely"
-import { Pool } from "pg"
-
-interface OccupationDataTable {
-  onetsoc_code: string
-  title: string
-  description: string
-}
-
-interface AlternateTitlesTable {
-  onetsoc_code: string
-  alternate_title: string
-  short_title: string | null
-  sources: string
-}
-
-interface Database {
-  occupation_data: OccupationDataTable
-  alternate_titles: AlternateTitlesTable
-}
-
-const { POSTGRES_CONNECTION_STRING } = process.env
-const db = new Kysely<Database>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      connectionString: POSTGRES_CONNECTION_STRING,
-    }),
-  }),
-  log:
-    process.env.NODE_ENV !== "production"
-      ? function (event) {
-          if (event.level === "query") {
-            console.log("SQL query executed.")
-            console.log(`Query: ${event.query.sql}`)
-            console.log(`Parameters: ${event.query.parameters}`)
-          }
-        }
-      : undefined,
-})
+import { postgresqlDb } from "./postgresql"
 
 export function getJobs({ limit, term }: { limit: number; term: string }) {
-  return db
+  return postgresqlDb
     .selectFrom("occupation_data")
     .innerJoin(
       "alternate_titles",
---
+++ src/server/postgresql.ts
@@ -0,0 +1,39 @@
+import { Kysely, PostgresDialect } from "kysely"
+import { Pool } from "pg"
+
+interface OccupationDataTable {
+  onetsoc_code: string
+  title: string
+  description: string
+}
+
+interface AlternateTitlesTable {
+  onetsoc_code: string
+  alternate_title: string
+  short_title: string | null
+  sources: string
+}
+
+interface Database {
+  occupation_data: OccupationDataTable
+  alternate_titles: AlternateTitlesTable
+}
+
+const { POSTGRES_CONNECTION_STRING } = process.env
+export const postgresqlDb = new Kysely<Database>({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      connectionString: POSTGRES_CONNECTION_STRING,
+    }),
+  }),
+  log:
+    process.env.NODE_ENV !== "production"
+      ? function (event) {
+          if (event.level === "query") {
+            console.log("SQL query executed.")
+            console.log(`Query: ${event.query.sql}`)
+            console.log(`Parameters: ${event.query.parameters}`)
+          }
+        }
+      : undefined,
+})
