0815fa8a812c339e870fdbf0fada67660ed5aed0
Angelo Geulin <angelogeulin123@gmail.com>
2023-07-06 20:11:48 +0800

    style: change footer and navbar to zinc+purple

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,19 @@ function Navbar() {
   const { status } = useSession()
 
   return (
-    <nav className="h-16 border-b border-gray-300 flex items-center px-6">
+    <nav className="h-16 border-b border-zinc-300 flex items-center px-6">
       <div className="max-w-6xl mx-auto w-full flex justify-between">
-        <h1 className="flex items-center text-lg text-gray-700 font-semibold">
+        <h1 className="flex items-center text-lg text-zinc-700 font-semibold">
           Job Descriptions
         </h1>
         <div>
           <button
             type="button"
             className={`transition duration-200 text-white font-medium w-20 py-2 rounded-md ${
-              status === "loading" ? "bg-blue-200" : ""
+              status === "loading" ? "bg-purple-300" : ""
             } ${
               status === "authenticated" || status === "unauthenticated"
-                ? "bg-blue-500 hover:bg-blue-400"
+                ? "bg-purple-600 hover:bg-purple-500"
                 : ""
             }`}
             onClick={() => {
@@ -56,7 +56,7 @@ export default function App({
         <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
           <Navbar />
           <Component {...pageProps} />
-          <footer className="bg-gray-800 text-white text-center font-medium px-4 py-3">
+          <footer className="bg-zinc-800 text-white text-center font-medium px-4 py-3">
             Angelo Geulin &copy; 2023
           </footer>
         </div>
