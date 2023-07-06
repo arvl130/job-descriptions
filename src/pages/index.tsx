b2190ce1c633c02fb0ed08974e14ae6d7629dadb
Angelo Geulin <angelogeulin123@gmail.com>
2023-07-06 20:58:02 +0800

    feat: turn index page into a landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,44 @@
-import { LandingPageForm } from "@/components/landing-page-form"
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 export default function Home() {
+  const { status } = useSession()
+  const router = useRouter()
+
   return (
-    <main className="max-w-lg mx-auto text-gray-700 py-12 px-6">
+    <main className="max-w-prose mx-auto text-zinc-700 py-12 px-6">
       <header className="pt-24 pb-6">
-        <h1 className="text-4xl font-bold text-center">
-          Job Description Search API
-        </h1>
+        <h1 className="text-5xl font-bold text-center">Job Descriptions</h1>
+        <p className="text-center mt-4 text-zinc-500">
+          Find all the job descriptions you need in just a few clicks.
+        </p>
+        <div className="flex justify-center gap-3 mt-12">
+          <button
+            type="button"
+            className={`
+            w-32 py-2 text-center text-white rounded-md transition duration-200 font-semibold
+            ${status === "loading" ? "bg-purple-300" : ""}
+            ${
+              status === "unauthenticated" || status === "authenticated"
+                ? "bg-purple-600 hover:bg-purple-500"
+                : ""
+            }
+            `}
+            onClick={() => {
+              if (status === "authenticated") router.push("/dashboard")
+              if (status === "unauthenticated") signIn()
+            }}
+          >
+            {status === "loading" && <br />}
+            {status === "unauthenticated" && <>Get started</>}
+            {status === "authenticated" && <>Dashboard</>}
+          </button>
+
+          <a href="#" className="w-32 py-2 text-center font-semibold">
+            Learn more →
+          </a>
+        </div>
       </header>
-      <LandingPageForm />
     </main>
   )
 }
