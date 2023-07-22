93c049eee2ff4f1daa5314372327811293c14ca5
Angelo Geulin <angelogeulin123@gmail.com>
2023-07-22 19:02:51 +0800

    fix: load credentials from env variables if endpoint url is not defined

--- src/server/dynamodb.ts
+++ src/server/dynamodb.ts
@@ -2,11 +2,26 @@ import { DynamoDB } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
 import { serverEnv } from "./env"
 
-const { AWS_REGION, DYNAMODB_ENDPOINT_URL } = serverEnv
-const dynamodb = new DynamoDB({
-  region: AWS_REGION,
-  endpoint: DYNAMODB_ENDPOINT_URL,
-})
+const {
+  AWS_REGION,
+  AWS_ACCESS_KEY_ID,
+  AWS_ACCESS_KEY_SECRET,
+  DYNAMODB_ENDPOINT_URL,
+} = serverEnv
+const dynamodb = new DynamoDB(
+  DYNAMODB_ENDPOINT_URL === undefined
+    ? {
+        region: AWS_REGION,
+        endpoint: DYNAMODB_ENDPOINT_URL,
+      }
+    : {
+        region: AWS_REGION,
+        credentials: {
+          accessKeyId: AWS_ACCESS_KEY_ID,
+          secretAccessKey: AWS_ACCESS_KEY_SECRET,
+        },
+      }
+)
 
 export const dynamodbDocument = DynamoDBDocument.from(dynamodb, {
   marshallOptions: {
