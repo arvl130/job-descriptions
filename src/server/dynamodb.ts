9dd58366ecaaa25c138499c08d446c58eb4fbc7f
Angelo Geulin <angelogeulin123@gmail.com>
2023-07-22 19:06:44 +0800

    fix: inverse logic

--- src/server/dynamodb.ts
+++ src/server/dynamodb.ts
@@ -12,14 +12,14 @@ const dynamodb = new DynamoDB(
   DYNAMODB_ENDPOINT_URL === undefined
     ? {
         region: AWS_REGION,
-        endpoint: DYNAMODB_ENDPOINT_URL,
-      }
-    : {
-        region: AWS_REGION,
         credentials: {
           accessKeyId: AWS_ACCESS_KEY_ID,
           secretAccessKey: AWS_ACCESS_KEY_SECRET,
         },
+      }
+    : {
+        region: AWS_REGION,
+        endpoint: DYNAMODB_ENDPOINT_URL,
       }
 )
 
